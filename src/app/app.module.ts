370e3b17a99a6dcd114277119e5bee9152e87532
vasanth <vk432@njit.edu>
2017-09-01 18:02:04 -0400

    adding question-page to ask and see list of questions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,25 @@ import { SignupComponent} from './auth/signup/signup.component';
 import { SigninComponent} from './auth/signin/signin.component';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthService } from './auth/auth.service';
+import { QuestionPageComponent } from './qn/question-page/question-page.component';
+import { QuestionListComponent } from './qn/question-list/question-list.component';
+import { AskQuestionComponent } from './qn/ask-question/ask-question.component';
 
 const appRoutes: Routes = [
   { path: 'sign-in', component: SigninComponent },
   { path: '', component: SignupComponent },
-  { path: 'sign-up', component: SignupComponent }
+  { path: 'sign-up', component: SignupComponent },
+  { path: 'question-page', component: QuestionPageComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     SigninComponent,
-    SignupComponent
+    SignupComponent,
+    QuestionPageComponent,
+    QuestionListComponent,
+    AskQuestionComponent
   ],
   imports: [
     BrowserModule,
