3e0a377548bbd001e9d901467ed16a2f309e22f8
vasanth <vk432@njit.edu>
2017-09-01 13:52:48 -0400

    created basic signin & signup page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { SignupComponent} from './auth/signup/signup.component';
+import { SigninComponent} from './auth/signin/signin.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'sign-in', component: SigninComponent },
+  { path: '', component: SignupComponent },
+  { path: 'sign-up', component: SignupComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
