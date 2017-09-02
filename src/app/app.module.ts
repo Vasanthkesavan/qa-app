d984209fae20762e6d4c08b1e49b27c80f3265ae
vasanth <vk432@njit.edu>
2017-09-02 15:00:25 -0400

    removed to start from scratch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,51 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { SignupComponent} from './auth/signup/signup.component';
-import { SigninComponent} from './auth/signin/signin.component';
-import { Routes, RouterModule } from '@angular/router';
-import { AuthService } from './auth/auth.service';
-import { QuestionPageComponent } from './qn/question-page/question-page.component';
-import { QuestionListComponent } from './qn/question-list/question-list.component';
-import { AskQuestionComponent } from './qn/ask-question/ask-question.component';
-import { EditorComponent } from './qn/ask-question/editor/editor.component';
-import { ServerService } from './server.service';
-import { HttpModule } from '@angular/http';
-import { HeadercomponentComponent } from './headercomponent/headercomponent.component';
-import { AuthGuard } from './auth/auth-guard.service';
-import { AnswerQuestionComponent } from './ans/answer-question/answer-question.component';
-import { QuestionDetailComponent } from './qn/question-list/question-detail/question-detail.component';
 
-const appRoutes: Routes = [
-  { path: 'sign-in', component: SigninComponent },
-  { path: '', component: SignupComponent },
-  { path: 'sign-up', component: SignupComponent },
-  { path: 'question-page', component: QuestionPageComponent, canActivate: [AuthGuard] },
-  { path: 'ask-question', component: AskQuestionComponent, canActivate: [AuthGuard] },
-  { path: 'answer-question', component: AnswerQuestionComponent, canActivate: [AuthGuard] }
-];
+import { Routes, RouterModule } from '@angular/router';
+
+
+import { HttpModule } from '@angular/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SigninComponent,
-    SignupComponent,
-    QuestionPageComponent,
-    QuestionListComponent,
-    AskQuestionComponent,
-    EditorComponent,
-    HeadercomponentComponent,
-    AnswerQuestionComponent,
-    QuestionDetailComponent,
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes),
     HttpModule
   ],
-  providers: [AuthService, ServerService, AuthGuard],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/qn/ask-question/question.structure.ts
+++
@@ -1,9 +0,0 @@
-export class QuestionStructure {
-    public questionName: string;
-    public textValue: string;
-
-    constructor(questionName, textValue) {
-        this.questionName = questionName;
-        this.textValue = textValue;
-    }
-}
