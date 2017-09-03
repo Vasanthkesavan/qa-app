279f4b453a2dd1ae4b1bb82640d97b66891c433d
vasanth <vk432@njit.edu>
2017-09-03 13:18:14 -0400

    added bootstrap nav and defined model for question

---
+++ src/app/questions/question.model.ts
@@ -0,0 +1,9 @@
+export class Question {
+    public questionName: string;
+    public questionDescription: string;
+
+    constructor(questionName: string, questionDescription: string) {
+        this.questionName = questionName;
+        this.questionDescription = questionDescription;
+    }
+}
